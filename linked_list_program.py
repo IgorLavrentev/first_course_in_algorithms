473e0bafe2af69a40fb281a5b497138b0887c6a5
IgorLavrentev <igor.lavrentev.bmstu@gmail.com>
2024-03-21 11:15:50 +0300

    fifth correction linked_list_program.py

--- linked_list_program.py
+++ linked_list_program.py
@@ -42,7 +42,7 @@ class LinkedList:
             node = node.next
         return final_list
 
-    def delete(self, val, all): # метод удаления всех узлов по конкретному значению
+    def delete(self, val, all=False): # метод удаления всех узлов по конкретному значению
         # исключение при пустом списке
         if self.head == None:
             return
@@ -63,13 +63,6 @@ class LinkedList:
         # проверка середины связанного списка и его крайнего правого элемента
         node = self.head
         while node is not None: # пока указатель node не равен None
-            if node.value == val and self.head.value == val:
-                node_previous.next = node.next
-                self.head = self.head.next
-                if all == False:
-                    return
-                node = node.next
-                continue
 
             # проверка последнего элемента списка
             if self.tail.value == val and node.next == None:
