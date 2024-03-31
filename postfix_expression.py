9f1efa2cabda2a17f0029eed60233ce022cb8411
IgorLavrentev <igor.lavrentev.bmstu@gmail.com>
2024-03-31 12:39:31 +0300

    second correction postfix_expression.py

--- postfix_expression.py
+++ postfix_expression.py
@@ -2,43 +2,40 @@ class Stack:
     def __init__(self):
         self.stack = []
 
-    def size(self): # метод длины стека
+    def size(self):  # метод длины стека
         return len(self.stack)
 
-    def pop(self): # метод удаления последнего добавленного элемента
+    def pop(self):  # метод удаления последнего добавленного элемента
         if len(self.stack) == 0:
-            return None # если стек пустой
+            return None  # если стек пустой
         el = self.stack[0]
         del self.stack[0]
         return el
 
-    def push(self, value): # метод добавления элемента
+    def push(self, value):  # метод добавления элемента
         self.stack = [value] + self.stack
 
-    def peek(self): # метод "возврата" последнего добавленного элемента
+    def peek(self):  # метод "возврата" последнего добавленного элемента
         if len(self.stack) == 0:
-            return None # если стек пустой
+            return None  # если стек пустой
         return self.stack[0]
 
-def postfix(entry_stack): # функция для реализации постфиксной записи выражения
+def postfix(entry_stack):  # функция для реализации постфиксной записи выражения
     second_stack = Stack()
-    for _ in range(entry_stack.size()): # "проход" по всему списку
-        if entry_stack.peek().isdigit(): # если очередное значение типа int
-            second_stack.push(entry_stack.pop()) # добавляем во второй список
+    for _ in range(entry_stack.size()):  # "проход" по всему списку
+        if entry_stack.peek().isdigit():  # если очередное значение типа int
+            second_stack.push(entry_stack.pop())  # добавляем во второй список
             continue
-
-        if entry_stack.peek() == '+': # если очередное значение "+"
-            entry_stack.pop() # удаление значения "+" из первого списка
-            # удаление двух последних добавленных значений второго списка
-            x = int(second_stack.pop())
-            y = int(second_stack.pop())
+        if entry_stack.peek() == "=": # возврат результата функции
+            return second_stack.peek()
+        x = int(second_stack.pop())
+        y = int(second_stack.pop())
+        if entry_stack.peek() == "+": # сложение
             second_stack.push(x + y)
-            continue
-
-        if entry_stack.peek() == '*': # если очередное значение "*"
-            entry_stack.pop() # удаление значения "*" из первого списка
-            # удаление двух последних добавленных значений второго списка
-            x = int(second_stack.pop())
-            y = int(second_stack.pop())
+        if entry_stack.peek() == "*": # умножение
             second_stack.push(x * y)
-            continue
+        if entry_stack.peek() == "-": # вычитание
+            second_stack.push(y - x)
+        if entry_stack.peek() == "/": # деления
+            second_stack.push(y / x)
+        entry_stack.pop()
