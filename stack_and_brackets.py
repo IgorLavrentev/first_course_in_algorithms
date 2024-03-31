f11479fbfb920f551eddd1613c1765520785e503
IgorLavrentev <igor.lavrentev.bmstu@gmail.com>
2024-03-31 12:35:18 +0300

    second correction stack_and_brackets.py

--- stack_and_brackets.py
+++ stack_and_brackets.py
@@ -7,7 +7,7 @@ class Stack:
 
     def pop(self):
         if len(self.stack) == 0:
-            return None # если стек пустой
+            return None  # если стек пустой
         el = self.stack[0]
         del self.stack[0]
         return el
@@ -17,22 +17,19 @@ class Stack:
 
     def peek(self):
         if len(self.stack) == 0:
-            return None # если стек пустой
+            return None  # если стек пустой
         return self.stack[0]
 
-def parentheses(string): # функция проверки сбалансированы ли скобки
-    st_1 = Stack() # стек левой скобки
-    st_2 = Stack() # стек правой скобки
-    i = 0 # счетчик для прохода по строке
+def parentheses(string):  # функция проверки сбалансированы ли скобки
+    st = Stack()
+    i = 0  # счетчик для прохода по строке
     while i < len(string):
-        if string[i] == '(':
-            st_1.push('(')
-        if string[i] == ')':
-            st_2.push(')')
-        if st_2.size() > st_1.size():
-            return False
-        if st_1.size() + st_2.size() == len(string) and st_1.size() == st_2.size():
+        if string[i] == "(": # если открывающая скобка добавляем
+            st.push("(")
+        if string[i] == ")": # если закрывающая скобка удаляем
+            st.pop()
+        if st.size() == 0 and i == len(string) - 1:
             return True
-        if st_1.size() + st_2.size() == len(string) and st_1.size() != st_2.size():
+        if st.size() != 0 and i == len(string) - 1:
             return False
         i += 1
