46bc25980e48fa65d1bca13516696076a260f3d2
IgorLavrentev <igor.lavrentev.bmstu@gmail.com>
2024-04-01 13:11:45 +0300

    add queue_program.py

---
+++ queue_program.py
@@ -0,0 +1,16 @@
+class Queue:
+    def __init__(self):
+        self.queue = [] # инициализация хранилища данных
+
+    def enqueue(self, item): # вставка в хвост
+        self.queue.append(item)
+
+    def dequeue(self): # выдача из головы
+        if self.queue is None:
+            return None # если очередь пустая
+        el = self.queue[0]
+        del self.queue[0]
+        return el
+
+    def size(self):
+        return len(self.queue) # размер очереди
