58c0c7efb883cde1aff5c80aa3b770b78ff4f2fa
IgorLavrentev <igor.lavrentev.bmstu@gmail.com>
2024-04-20 10:18:56 +0300

    second correction Blum_filter_program.py

--- Blum_filter_program.py
+++ Blum_filter_program.py
@@ -4,29 +4,28 @@ from bitarray import bitarray
 class BloomFilter:
 
     def __init__(self, f_len):
-        self.filter_len = f_len
-        # создаём битовый массив длиной f_len
+        self.filter_len = f_len  # creating a bit array of length f_len
         self.bloom_array = bitarray(f_len)
 
     def hash1(self, str1):
-        code_previous = 0  # предыдущий результат
+        code_previous = 0  # previous result
         for c in str1:
             code = code_previous * 17 + ord(c)
             code_previous = code
         return code % self.filter_len
 
     def hash2(self, str1):
-        code_previous = 0  # предыдущий результат
+        code_previous = 0  # previous result
         for c in str1:
             code = code_previous * 223 + ord(c)
             code_previous = code
         return code % self.filter_len
 
-    def add(self, str1):  # добавляем строку str1 в фильтр
+    def add(self, str1):  # adding a line str1 to the filter
         self.bloom_array[self.hash1(str1)] = 1
         self.bloom_array[self.hash2(str1)] = 1
 
-    def is_value(self, str1):  # проверка, имеется ли строка str1 в фильтре
+    def is_value(self, str1):  # checking whether the string str1 is in the filter
         if (
             self.bloom_array[self.hash1(str1)] == 1
             and self.bloom_array[self.hash2(str1)] == 1
